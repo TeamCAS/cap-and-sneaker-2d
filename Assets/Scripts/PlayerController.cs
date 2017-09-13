aa908f49c4d302cceeaccec44fb4486975a6038b
Nathan Acosta <logic0verflow90@gmail.com>
2017-09-12 20:21:04 -0600

    Update controls so the player must release jump before jumping again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float runMultiplier = 100;
-    public float maxRunSpeed = 15;
+    public float maxRunSpeed = 20;
     public float jumpStrength = 1000;
 
     Rigidbody2D rbody;
     GroundCheck groundCheck;
+    bool canJump= false;
 
 
 	// Use this for initialization
@@ -35,8 +36,15 @@ public class PlayerController : MonoBehaviour {
     // has been detected and if the player is on the ground
     void Jump() {
         float jumpInput = GameManager.InputHandler.jump;
-        if (jumpInput != 0 && groundCheck.isGrounded()) {
+
+        // If input is not 1, the player released the button, they can jump
+        if (jumpInput != 1) canJump = true;
+
+        // Player can jump as long as they are grounded and weren't holding
+        // down jump i.e. canJump is true
+        if (jumpInput != 0 && groundCheck.isGrounded() && canJump) {
             rbody.AddForce(new Vector2(0, jumpStrength));
+            canJump = false;
         }
     }
 }
