c5b5baec86b7e4dc1142498bad430a5f517f70ee
Nathan Acosta <logic0verflow90@gmail.com>
2017-09-12 20:35:19 -0600

    Created CameraAnchor attached to main camera that follows a transform

---
+++ Assets/Scripts/CameraAnchor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnchor : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            player.position.x,
+            player.position.y,
+            player.position.z - 10
+        );
+	}
+}
