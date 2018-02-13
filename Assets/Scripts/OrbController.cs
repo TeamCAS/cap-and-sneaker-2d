f3f498ce78e9105287b88981710616d11f1f2c0d
Nathan Acosta <git@nathanacosta.com>
2018-02-12 19:12:44 -0700

    feat: new idle pic, include blue spheres with there own sfx

--- Assets/Scripts/OrbController.cs
+++ Assets/Scripts/OrbController.cs
@@ -6,10 +6,17 @@ public class OrbController : MonoBehaviour {
 
     // Time delay before destroying this orbs gameobject
     public float destroyDelayMs = 3;
-    
+
+    AudioSource audio;
+
+    private void Start() {
+        audio = GameObject.Find("OrbSFX").GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             GameManager.DataHandler.incrementOrbCount();
+            audio.PlayOneShot(audio.clip);
             Destroy(gameObject, destroyDelayMs);
             gameObject.SetActive(false);
         }
